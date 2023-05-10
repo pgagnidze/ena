7446179b3da59ae8737ee3d90386d412da27dd7d
pgagnidze <pgagnidze@pm.me>
2023-05-10 20:24:30 +0400

    fix: clean unused functions

--- src/compiler.lua
+++ src/compiler.lua
@@ -9,26 +9,6 @@ local Compiler = {
     nvars = 0
 }
 
-local function pt(x, id, visited)
-    visited = visited or {}
-    id = id or ""
-    if type(x) == "string" then
-        return "'" .. tostring(x) .. "'"
-    elseif type(x) ~= "table" then
-        return tostring(x)
-    elseif visited[x] then
-        return "..."
-    else
-        visited[x] = true
-        local s = id .. "{\n"
-        for k, v in pairs(x) do
-            s = s .. id .. tostring(k) .. " = " .. pt(v, id .. "  ", visited) .. ";\n"
-        end
-        s = s .. id .. "}"
-        return s
-    end
-end
-
 local toName = {
     [lop.add] = "add",
     [lop.subtract] = "subtract",
